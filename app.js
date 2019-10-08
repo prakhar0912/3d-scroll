613bd09f89582ecd6e52f8b722c586d8a9c7eb89
Prakhar Soni <prakhar0912@gmail.com>
2019-10-08 20:15:25 +0530

    funky text

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const box = document.querySelector('.box');
 const body = document.querySelector('body');
+
+
 i=0;
+
+window.onload = ()=> {
+    let okay  = document.querySelector('.one');
+    const arr = ['okay','@kay','0&ay','ok6y','oka9'];
+let o  = okay[0];
+let m=0;
+setInterval(()=>{
+     okay.innerHTML= arr[m] ;
+     m++;
+     if(m===4){
+         m=0;
+     }
+},200);
+};
 
 addEventListener('mousewheel', (e)=>{
     if(i>2700){
