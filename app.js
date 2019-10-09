02cb87c7788fac41bd47dd8034e46bd98332bd81
Prakhar Soni <prakhar0912@gmail.com>
2019-10-09 12:45:43 +0530

    space, music ,

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const okay  = document.querySelector('.one');
 const five = document.querySelector('.five');
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
+const audio = document.querySelector('audio');
 
 
-const arr = ['okay','@kay','0&ay','ok6y','oka9'];
+const arr = ['Space','Space <br> <br><span id="subtitle" style="font-size:0.3rem;font-style:italic; ">The New Frontier<span>','0&ay','ok☺️y','oka9'];
 const zposition = [0, 1400,2520];
 
 TweenLite.fromTo(".one", 1, {
     transform: "translate3d(-50%,-50%,3200px)"
 },{
-    transform: "translate3d(-50%,-50%,2500px)"
+    transform: "translate3d(-50%,-50%,2500px)"  
 });
+audio.pause();
+
+
 
 
 
@@ -29,26 +33,33 @@ okay.addEventListener('mouseover',()=>{
             m = 0;
             clearInterval(a);
         }
-    },150);
+    },600);
     
 })
 
 // traversing z-axis
 i=0;
 addEventListener('mousewheel', (e)=>{
+    audio.play();
     if(i>2500){
+        body.style.overflowY = 'scroll';
+        body.style.overflowX = 'hidden'; 
         five.style.top = 0;
         five.style.left = 0;
         five.style.transform = 'translate3d(0,0,0px)';
         removeEventListener();
     }
+    
     if (e.wheelDelta<=0){     
         i+=40;
     }
     else{          
         i-=40;
+        audio.pause();
+        
     }
-    box.style.transform ='translateZ('+i+'px)';  
+    box.style.transform ='translateZ('+i+'px)';
+  
     console.log(i)
 })
 
@@ -60,7 +71,7 @@ next.addEventListener('click', ()=>{
 
     for (i = 0;i<3;i++){
         if (i == n){
-            TweenLite.to(box, 2, {
+            TweenLite.to(box, 1, {
                 transform : 'translateZ('+zposition[i]+'px)'
             });
             
